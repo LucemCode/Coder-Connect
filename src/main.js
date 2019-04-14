179b0188020adca03594275feaeaaacdf7b16a1f
LucemCode <pascal.oberbeck99@gmail.com>
2019-04-14 20:55:51 +0200

    Add copy and past & travis.yml

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 let win;
 
 function createWindow() {
     win = new BrowserWindow({ width: 600, height: 400, titleBarStyle: 'hidden' });
+    new Menu();
     win.setOpacity(0.98);
 
     win.loadFile(`${__dirname}/index.html`);
@@ -14,6 +15,38 @@ function createWindow() {
     win.on('closed', () => {
         win = null;
     });
+
+    var template = [
+        {
+            label: 'Application',
+            submenu: [
+                { label: 'About Application', selector: 'orderFrontStandardAboutPanel:' },
+                { type: 'separator' },
+                {
+                    label: 'Quit',
+                    accelerator: 'Command+Q',
+                    click: function() {
+                        app.quit();
+                    }
+                }
+            ]
+        },
+        {
+            label: 'Edit',
+            submenu: [
+                { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+                { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+                { type: 'separator' },
+                { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+                { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+                { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+                { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+            ]
+        }
+    ];
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
 }
 
 app.on('ready', createWindow);
